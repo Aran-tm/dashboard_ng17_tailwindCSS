3e99a2fad60028b07ff0c7ad162c0f60f4bfbfac
Aran-tm <armandortmontes@gmail.com>
2024-04-30 13:07:18 -0500

    estructura if en ejemplo

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-control-flow',
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./control-flow.component.css']
 })
 export default class ControlFlowComponent implements OnInit {
+
+  // propiedades de clase
+  public showContent = signal(false);
+
+  public toggleContent() {
+
+    // cuando hago click en el valor del boton, automaticamente cambia su valor booleano
+    // funcion update y se pasa por parametro un valor value
+    this.showContent.update(value => !value);
+  }
 
   constructor() { }
 
