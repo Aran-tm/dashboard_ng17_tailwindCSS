2816b57bdd75ed26cb6c6bc72b4cb5375806b524
Aran-tm <armandortmontes@gmail.com>
2024-04-30 11:02:01 -0500

    creando y configurando los Alias Paths en la aplicacion SPA

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { SidemenuComponent } from '../shared/sidemenu/sidemenu.component';
+import { SidemenuComponent } from '@shared/sidemenu/sidemenu.component';    // creando alias
 
 @Component({
   selector: 'app-dashboard',
