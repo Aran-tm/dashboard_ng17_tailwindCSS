aea38910d9abb9201b238ceb04caa6c3b4d96b53
Aran-tm <armandortmontes@gmail.com>
2024-05-02 09:18:08 -0500

    updating pipe view

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,21 +1,26 @@
 import { JsonPipe } from './change-detection.pipe';
-import { Component, OnInit, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, OnInit, signal } from '@angular/core';
 import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   selector: 'app-change-detection',
   standalone: true,
   imports: [TitleComponent, JsonPipe],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-    <app-title title="Change Detection"/>
+    <app-title [title]="currentFramework()"/>
 
-    <pre> {{ frameworkAsSingal() | json }} </pre>
+    <pre> {{ frameworkAsSignal() | json }} </pre>
     <pre> {{ frameworkAsProperty | json }} </pre>
   `,
 })
 export default class ChangeDetectionComponent implements OnInit {
 
-  public frameworkAsSingal = signal({
+  public currentFramework = computed(
+    ()=> `Change Detection - ${this.frameworkAsSignal().name}`
+  )
+
+  public frameworkAsSignal = signal({
     name: 'Angular',
     release: '2016'
   });
@@ -25,7 +30,21 @@ export default class ChangeDetectionComponent implements OnInit {
     release: '2016'
   };
 
-  constructor() { }
+  constructor() {
+
+    // este es un setTimeout para esperar y mostrar las cosas
+    setTimeout(() => {
+
+      //this.frameworkAsProperty.name = 'React';
+      // esto me permite actualizar el signal despues de los 3 segundos
+      this.frameworkAsSignal.update(value => ({
+        ...value,
+        name: 'React'
+      }))
+
+      console.log(`Hecho`);
+    }, 3000);
+  }
 
   ngOnInit() {
   }
--- src/app/dashboard/pages/change-detection/change-detection.pipe.ts
+++ src/app/dashboard/pages/change-detection/change-detection.pipe.ts
@@ -1,9 +1,15 @@
+
+// los pipes en Angular son una forma para poder transofrmar datos antes de presentarlos en la vista
+// I -> PipeTransform, Metodo -> transform
 import { Pipe, PipeTransform } from '@angular/core';
 
+// este es el decorador @Pipe con nombre json y standalone
 @Pipe({
   name: 'json',
   standalone: true
 })
+
+// esta es la exportacion de la clase JsonPipe la cual implementa de la clase PipeTransform
 export class JsonPipe implements PipeTransform {
   transform(value: any): string {
     return JSON.stringify(value, null, 2);
