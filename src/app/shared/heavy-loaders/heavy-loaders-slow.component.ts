3b640f5d0e7d3052b6db1c16d5811cb9fa7b18d7
Aran-tm <armandortmontes@gmail.com>
2024-05-02 13:02:44 -0500

    agregando defer

--- src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
+++ src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
@@ -1,13 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
 
+// hay que imprtar el common module para que detecte el ngClass
 @Component({
   standalone: true,
   selector: 'app-heavy-loaders-slow',
+  imports: [CommonModule],
   template: `
-    <h1>Hola Mundo</h1>
+    <section [ngClass]="['w-full h-[600px]', cssClass]">
+      Heavy Loader Slow
+    </section>
   `
 })
 export class HeavyLoadersSlowComponent implements OnInit {
+
+  @Input({ required: true }) cssClass!: string;
 
   constructor() {
     console.log(`Heavy Loader Component`);
