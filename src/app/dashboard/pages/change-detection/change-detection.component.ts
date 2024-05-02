6f56491912c854b46d3507f9da347ce9d071406d
Aran-tm <armandortmontes@gmail.com>
2024-05-02 08:40:49 -0500

    agregando mi primer pipe para poder leer archivos json

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,12 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { JsonPipe } from './change-detection.pipe';
+import { Component, OnInit, signal } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   selector: 'app-change-detection',
   standalone: true,
-  templateUrl: './change-detection.component.html',
-  styleUrls: ['./change-detection.component.css']
+  imports: [TitleComponent, JsonPipe],
+  template: `
+    <app-title title="Change Detection"/>
+
+    <pre> {{ frameworkAsSingal() | json }} </pre>
+    <pre> {{ frameworkAsProperty | json }} </pre>
+  `,
 })
 export default class ChangeDetectionComponent implements OnInit {
+
+  public frameworkAsSingal = signal({
+    name: 'Angular',
+    release: '2016'
+  });
+
+  public frameworkAsProperty = {
+    name: 'Angular',
+    release: '2016'
+  };
 
   constructor() { }
 
